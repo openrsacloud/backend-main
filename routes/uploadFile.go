f48dda21ddc92fc8d1888f9afa260b404c433fe9
varma02 <varma01@mail.com>
2024-01-27 00:49:26 +0100

    file upload 🎉

---
+++ routes/uploadFile.go
@@ -0,0 +1,73 @@
+package routes
+
+import (
+	"io"
+	"log"
+	"openrsacloud/backend/db"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/surrealdb/surrealdb.go"
+)
+
+func UploadFile(c *fiber.Ctx) error {
+	sessionData := c.Locals("session").(db.Session)
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status":  400,
+			"message": "Bad Request",
+			"info":    "Failed to parse form data",
+		})
+	}
+
+	_newFileOptions := fiber.Map{
+		"name":  c.FormValue("filename"),
+		"owner": sessionData.User,
+		"size":  file.Size,
+		"type":  file.Header.Get("Content-Type"),
+	}
+	if c.FormValue("parent") != "" {
+		_newFileOptions["parent"] = c.FormValue("parent")
+	}
+	resp, err := db.DB.Create("files", _newFileOptions)
+	if err != nil {
+		return err
+	}
+
+	log.Println(resp)
+
+	var fileData []db.File
+	err = surrealdb.Unmarshal(resp, &fileData)
+	if err != nil {
+		return err
+	}
+	log.Println(fileData)
+
+	src, err := file.Open()
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status":  400,
+			"message": "Bad Request",
+			"info":    "Failed to open file",
+		})
+	}
+	defer src.Close()
+
+	destination, err := os.Create(os.Getenv("BasePath") + fileData[0].Id[7:])
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, src)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status":  200,
+		"message": "OK",
+	})
+}
